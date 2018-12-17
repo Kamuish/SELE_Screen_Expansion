a975ef4b773a6c16d0224272cd4c13c3ce864e90
Kamuish <andremiguel952@gmail.com>
2018-12-17 18:47:56 +0000

    Removed redundant folder

--- UART/Release/USART_comms.d
+++
@@ -1,4 +0,0 @@
-USART_comms.d: ../USART_comms.c \
- /home/joaorodrigues/Desktop/SELE/atmega368p_USART/USART_comms.h
-
-/home/joaorodrigues/Desktop/SELE/atmega368p_USART/USART_comms.h:
--- UART/Release/main.d
+++
@@ -1,4 +0,0 @@
-main.d: ../main.c \
- /home/joaorodrigues/Desktop/SELE/atmega368p_USART/USART_comms.h
-
-/home/joaorodrigues/Desktop/SELE/atmega368p_USART/USART_comms.h:
