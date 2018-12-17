30ce99634ebdcf7a5417753d35ba0b3ad8e72619
Kamuish <andremiguel952@gmail.com>
2018-12-17 17:06:29 +0000

    working version

--- src/flash_tests/main.c
+++ src/flash_tests/main.c
@@ -8,17 +8,32 @@
 
 #include "main.h"
 
+
+
 int main(void){
+
 	uart_init();
 	stdout = &uart_output;
 
-	uint8_t result = 0;
-	//result = flash_teste();
+	_delay_ms(1000);
+	puts("Starting flash test");
+	uint8_t result = flash_teste();
 
-	while(1){
-		puts("aaa");
-		_delay_ms(2000);
+	if (1 == result){
+		puts("flash test passed");
+		PORTB = (1<<PB0);
+
+	DDRB = (1<<DDB1)|(1<<DDB0);
 	}
+	else{
+		puts("flash test failed");
+		PORTB = (1<<PB1);
 
+			DDRB = (1<<DDB1)|(1<<DDB0);
+	}
+	while(1){
+
+		_delay_ms(1000);
+	}
 	return 0;
 }
