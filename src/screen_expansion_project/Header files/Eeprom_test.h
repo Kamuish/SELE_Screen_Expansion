cb578cb78d0c9d3f8a8dd4e4f086f24c17a2dc41
Kamuish <andremiguel952@gmail.com>
2018-12-18 22:57:17 +0000

    Eeprom tests

---
+++ src/screen_expansion_project/Header files/Eeprom_test.h
@@ -0,0 +1,22 @@
+/*
+ * Eeprom_test.h
+ *
+ *  Created on: Dec 18, 2018
+ *      Author: andre
+ */
+
+#ifndef HEADER_FILES_EEPROM_TEST_H_
+#define HEADER_FILES_EEPROM_TEST_H_
+
+#include <avr/io.h>
+#include <avr/eeprom.h>
+#include <util/delay.h>
+
+#define N_BYTES_EEPROM 1024
+#define USED_ADDR                0x22   /* Address used in the EEPROM memory */
+
+/* Function definition */
+void Eeprom_test(void);
+
+
+#endif /* HEADER_FILES_EEPROM_TEST_H_ */
---
+++ src/screen_expansion_project/Program files/Eeprom_test.c
@@ -0,0 +1,68 @@
+
+#include <Eeprom_test.h>
+
+
+uint8_t Eeprom_test(void)
+{
+	/*
+	 * Test to verify the correct functioning of the Eeprom memory.
+	 * First we write zero to all bytes, to ensure that there is no data in the eeprom
+	 * Afterwards, we write one to all bytes and, if the memory is working properly, we should read 1's on all
+	 * Then we repeat the process for zeros.
+	 * After passing all tests we restore the previously stored data
+	 */
+    uint16_t real_hash = eeprom_read_word( (const uint16_t*) USED_ADDR); /* guardar a hash que está na eeprom */
+    uint8_t tmp = 0;
+    uint8_t errors = 0;
+    for (int k =0; k <N_BYTES_EEPROM ; k++)
+    {
+        eeprom_write_byte((uint8_t*) k , 0x00);
+    }
+    for(int k =0; k <N_BYTES_EEPROM; k++)
+    {
+    	tmp = eeprom_read_byte((uint8_t*) k);
+        if (0 == tmp)
+        {
+            eeprom_write_byte((uint8_t*) k ,0xFF);
+        }
+        else
+        {
+        	errors++;
+            eeprom_write_byte((uint8_t*) k , 0xFF);
+        }
+    }
+
+
+    for(int k = 0; k < N_BYTES_EEPROM ; k++)
+    {
+    	tmp=eeprom_read_byte((uint8_t*) k);
+        if (0xFF == tmp)
+        {
+            eeprom_write_byte((uint8_t*) k ,0x00);
+        }
+        else
+        {
+        	errors++;
+            eeprom_write_byte((uint8_t*) k, 0x00);
+        }
+    }
+
+    for (int k =0; k < N_BYTES_EEPROM; k++)
+    {
+    	tmp =eeprom_read_byte((uint8_t*) k );
+        if(0 != tmp)
+        {
+        	errors++;
+        }
+    }
+    eeprom_write_word(( uint16_t*) USED_ADDR, real_hash);
+    if (0 == errors)
+    {
+    	return 0;
+    }
+    else
+    {
+    	return 1;
+    }
+
+}
