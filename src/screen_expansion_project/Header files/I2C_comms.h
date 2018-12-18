39c4f450b01d8f6b94b34f28abfca9e6f8b4b013
Kamuish <andremiguel952@gmail.com>
2018-12-18 15:41:21 +0000

    Style

--- src/screen_expansion_project/Header files/I2C_comms.h
+++ src/screen_expansion_project/Header files/I2C_comms.h
@@ -16,10 +16,10 @@
 #define I2C_ADDR   0x40
 
 /* I2C functions  */
-void 	I2C_Init(void);					/* Set up I2C communication */
-void 	I2C_Start(unsigned char addr);	/* Starts I2C communication */
+void 	I2C_Init(void);					    /* Set up I2C communication */
+void 	I2C_Start(unsigned char addr);	    /* Starts I2C communication */
 uint8_t I2C_Write(unsigned char data_i2c);	/* Writes data through I2C */
-void  	I2C_Stop(void);					/* Stops I2C communication */
+void  	I2C_Stop(void);					    /* Stops I2C communication */
 
 /* Utility functions */
 void I2C_WaitForTwint(void);
