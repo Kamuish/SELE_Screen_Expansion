ea8f501ec791c0d0452a4a23c6fdde45413701c1
Kamuish <andremiguel952@gmail.com>
2018-12-20 11:05:16 +0000

    SPI DEMO

--- src/SPI/SPI_demo/main.c
+++ src/SPI/SPI_demo/main.c
@@ -9,8 +9,14 @@
 uint8_t main(void){
 
 	SPI_MasterInit();
-	uint8_t data = 0x00;
+	uint8_t data = 0xF0;
 	SPI_MasterTransmit(data);
+	data = 0x2A;
+	SPI_MasterTransmit(data);
+	data = 0xFF;
+	SPI_MasterTransmit(data);
+	uint8_t aaa = 'A';
+	SPI_MasterTransmit(aaa);
 
 	return 0;
 }
